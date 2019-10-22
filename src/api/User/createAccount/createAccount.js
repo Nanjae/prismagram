8a1d1c66dcbdaf0d17174bea80ba63486716ddaa
Nanjae <33223057+Nanjae@users.noreply.github.com>
2019-10-22 22:18:31 +0900

    Refactoring for Frontend Authorization

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,21 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const existingUser = await prisma.$exists.user({ username });
+      const existingEmail = await prisma.$exists.user({ email });
+      if (existingUser) {
+        throw Error("이미 등록된 사용자 이름 입니다.");
+      } else if (existingEmail) {
+        throw Error("이미 등록된 이메일 입니다.");
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,16 +4,13 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
-      console.log(request.user);
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
-        console.log(error);
+      } catch {
         return false;
       }
     }
