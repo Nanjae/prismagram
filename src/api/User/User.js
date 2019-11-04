04fa41048abee70734e25cb14778d956240631aa
Nanjae <dngngn3045@gmail.com>
2019-11-05 07:27:45 +0900

    Update seeUser and User

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -2,6 +2,27 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   User: {
+    following: ({ id }) => prisma.user({ id }).following(),
+    followers: ({ id }) => prisma.user({ id }).followers(),
+    posts: ({ id }) => prisma.user({ id }).posts(),
+    likes: ({ id }) => prisma.user({ id }).likes(),
+    comments: ({ id }) => prisma.user({ id }).comments(),
+    rooms: ({ id }) => prisma.user({ id }).rooms(),
+    followingCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { followers_some: { id } } })
+        .aggregate()
+        .count(),
+    followerCount: ({ id }) =>
+      prisma
+        .usersConnection({ where: { following_some: { id } } })
+        .aggregate()
+        .count(),
+    postsCount: ({ id }) =>
+      prisma
+        .postsConnection({ where: { user: { id } } })
+        .aggregate()
+        .count(),
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
